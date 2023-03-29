0c419b1f4f5a3b65b722509b0d04e05c6c7c0e10
Iryna Abrosimova <iryna.abrosimova@cxdisruption.com>
2023-03-29 13:50:23 +0200

    feat: add lesson 13 react fraagment

---
+++ lesson-13/src/components/Card.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+
+const Card = () => {
+  return (
+    // <React.Fragment></React.Fragment>
+    <>
+      <h1>Irene</h1>
+      <h3>I'm learning React</h3>
+      <button>Like!</button>
+    </>
+  );
+};
+
+export default Card;
